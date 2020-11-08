8252c76a5158d22bfcc72c11b23cbbf826a833b7
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-11-07 22:28:20 -0500

    Add merge and global css utils

--- css.js
+++ css.js
@@ -2,15 +2,29 @@ import { serializeStyles } from "https://cdn.skypack.dev/@emotion/serialize";
 import { compile, serialize, stringify } from "https://cdn.skypack.dev/stylis";
 import { extractProps } from "./ssg.js";
 
-export default function (...args) {
+export function css(...args) {
   const { name, styles } = serializeStyles(args);
-  const className = `ssg-${name}`;
+  const className = `class-${name}`;
   return { class: className, css: `.${className} {${styles}}` };
 }
 
 export function global(...args) {
-  const { styles } = serializeStyles(...args);
+  const { styles } = serializeStyles(args);
   return { css: styles };
+}
+
+export function merge(...args) {
+  const classNames = [];
+  const cssFragments = [];
+  for (const arg of args) {
+    if (arg.class) {
+      classNames.push(arg.class);
+    }
+    if (arg.css) {
+      cssFragments.push(arg.css);
+    }
+  }
+  return { class: classNames.join(" "), css: cssFragments.join(" ") };
 }
 
 export async function extract(data) {
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import css from "../css.js";
+import { css, merge, global } from "../css.js";
 
 export default ({ items }) => {
   return {
     tag: "div",
-    ...css({
-      display: "flex",
-      flexDirection: "column",
-      background: "#eee",
-      "@media (min-width: 100px)": { a: { color: "red" } },
-    }),
+    ...merge(
+      css({
+        display: "flex",
+        flexDirection: "column",
+      }),
+      global({ div: { border: "solid red 1px" } })
+    ),
     inner: items.map(({ title, slug }) => ({
       tag: "a",
       href: `/${slug}.html`,
